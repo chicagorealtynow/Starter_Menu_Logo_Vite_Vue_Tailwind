4bda07463ccea845e88096a53690375c42c95db7
Greg Wilson <chicagorealtynow@gmail.com>
2024-11-12 19:43:45 -0500

    Add project files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+     <NavBar />
+</div> 
+</template>
+
+<script setup>
+ import { defineAsyncComponent } from 'vue';
+//Import Components Asynchronously
+const NavBar = defineAsyncComponent(()=>import('@/components/NavBar.vue'));
+
+</script>
+
+<style>
+
+*{
+  scrollbar-width: thin;
+  scrollbar-color: #111827 #f1f1f1;
+}
+</style>
